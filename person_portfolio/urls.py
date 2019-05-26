698f21973a2d5465aea7578f72a0b51d00bfc08a
henrymbuguak <henrymbuguak@gmail.com>
2019-05-26 14:31:39 +0300

    abilit to login user

--- person_portfolio/urls.py
+++ person_portfolio/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.sign_up, name='user_sign'),
-    path('dashboard', views.user_dashboard, name='user_dashboard')
+    path('dashboard', views.user_dashboard, name='user_dashboard'),
+    path('login', views.sign_in, name='login_user'),
 ]
--- person_portfolio/views.py
+++ person_portfolio/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
 def sign_up(request):
@@ -13,11 +14,31 @@ def sign_up(request):
             password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
             login(request, user)
-            return HttpResponse('A new user has been successfully registered!')
+            return redirect('user_dashboard')
     else:
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
 
 
+@login_required()
 def user_dashboard(request):
     return render(request, 'dashboard.html')
+
+
+def sign_in(request):
+    msg = []
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect('user_dashboard')
+            else:
+                msg.append('You account has been deactivated!')
+    else:
+        msg.append('Invalid Login credentials, try again!')
+    return render(request, 'login.html', {'errors': msg})
+
