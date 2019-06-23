0cc68404851fac23814aa4064855958463906ffb
henrymbuguak <henrymbuguak@gmail.com>
2019-06-23 09:50:25 +0300

    Ability to deactivate user

--- person_portfolio/urls.py
+++ person_portfolio/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('dashboard', views.user_dashboard, name='user_dashboard'),
     path('login', views.sign_in, name='login_user'),
     path('users', views.registered_users, name='system_users'),
+    path('activate/user/<int:user_id>', views.user_activate, name='activate_user'),
+    path('deactivate/user/<int:user_id>', views.user_deactivate, name='deactivate_user'),
 ]
--- person_portfolio/views.py
+++ person_portfolio/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import login, authenticate
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 def sign_up(request):
@@ -54,3 +55,19 @@ def registered_users(request):
     return render(request, 'users.html', context)
 
 
+@login_required()
+def user_deactivate(request, user_id):
+    user = User.objects.get(pk=user_id)
+    user.is_active = False
+    user.save()
+    messages.success(request, "User account has been successfully deactivated!")
+    return redirect('system_users')
+
+
+@login_required()
+def user_activate(request, user_id):
+    user = User.objects.get(pk=user_id)
+    user.is_active = True
+    user.save()
+    messages.success(request, "User account has been successfully activated!")
+    return redirect('system_users')
